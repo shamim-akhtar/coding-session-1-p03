d10fe406764de8b0dbeff65ed3315120dc548785
Shamim Akhtar <shamim.akhtar@gmail.com>
2022-01-12 15:35:09 +0800

    Implemented 3 types of third person cameras.
    1. track, track pos and track pos & rot.
    2. added the flexibility to change cameras at runtime
    3. added the flexibility to change game constant values at runtime.

---
+++ Session1/Assets/Scripts/TPC/TPCBase.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+namespace TPC
+{
+
+  /// <summary>
+  /// This class is the base class for all 
+  /// third person camera controls.
+  /// Remember these classes are NOT monobehavior classes
+  /// and hence cannot be added to the GameObject as component.
+  /// Instead we will instantiate the concrete implementation of
+  /// these third person cameras in our CThirdPersonCamera monobehavior class.
+  /// </summary>
+  public abstract class TPCBase
+  {
+    /// <summary>
+    /// For a third person camera to work it only 
+    /// required the following two transforms.
+    /// </summary>
+    protected Transform mCameraTransform;
+    protected Transform mPlayerTransform;
+
+    // the constructor.
+    public TPCBase(Transform cameraTranform, Transform playerTransform)
+    {
+      mCameraTransform = cameraTranform;
+      mPlayerTransform = playerTransform;
+    }
+
+    public abstract void Frame();
+  }
+}
---
+++ Session1/Assets/Scripts/TPC/TPCFollowTrackPosition.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+namespace TPC
+{
+  public class TPCFollowTrackPosition : TPCFollow
+  {
+    public TPCFollowTrackPosition(Transform cameraTranform, Transform playerTransform)
+    : base(cameraTranform, playerTransform)
+    {
+
+    }
+
+    public override void Frame()
+    {
+      // Create the initial rotation quaternion based on the 
+      // camera angle offset.
+      Quaternion initialRotation =
+         Quaternion.Euler(GameConstants.CameraAngleOffset);
+
+      // Now rotate the camera to the above initial rotation offset.
+      // We do it using damping/Lerp
+      // You can change the damping to see the effect.
+      mCameraTransform.rotation =
+          Quaternion.RotateTowards(mCameraTransform.rotation,
+              initialRotation,
+              Time.deltaTime * GameConstants.Damping);
+
+      base.Frame();
+    }
+  }
+}
